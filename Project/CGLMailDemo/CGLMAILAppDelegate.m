0542e472297c60fe41b4e9c56360e59611acd4ea
Chris Ladd <Christopher.Ladd@nytimes.com>
2014-02-15 17:06:19 -0500

    restores demo code

--- Project/CGLMailDemo/CGLMAILAppDelegate.m
+++ Project/CGLMailDemo/CGLMAILAppDelegate.m
@@ -16,13 +16,10 @@
 
     self.window.rootViewController = [[UIViewController alloc] init];
     
-//    UIViewController *mailVC = [CGLMailHelper supportMailViewControllerWithRecipient:@"support@support.com" subject:@"Support Email" completion:^(NSString *result, NSError *error) {
-//        NSLog(@"%s: %@, %@", __PRETTY_FUNCTION__, result, error);
-//    }];
+    UIViewController *mailVC = [CGLMailHelper supportMailViewControllerWithRecipient:@"support@support.com" subject:@"Support Email" completion:^(NSString *result, NSError *error) {
+        NSLog(@"%s: %@, %@", __PRETTY_FUNCTION__, result, error);
+    }];
 
-    UIViewController *mailVC = [CGLMailHelper mailViewControllerWithRecipients:@[@"my@mom.com"] subject:@"Hi Mom!" message:@"Hi Mom, \n\nJust wanted to check in and say hello!\n\nLove, \nChris" isHTML:NO includeAppInfo:NO completion:nil];
-
-    
     [self.window.rootViewController presentViewController:mailVC animated:YES completion:NULL];
     
     [self.window makeKeyAndVisible];
